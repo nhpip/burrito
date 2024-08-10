5b06be10f43249cc9be1aa412ba1bb83e4b98253
nhpip <nhpip@github.com>
2024-08-10 13:46:22 -0400

    wip

--- src/erlang_launcher.zig
+++ src/erlang_launcher.zig
@@ -62,7 +62,7 @@ pub fn launch(install_dir: []const u8, env_map: *EnvMap, meta: *const MetaStruct
         "-extra",
     };
 
-   var dir = try fs.cwd().openIterableDir(".", .{});
+   var dir = try std.fs.openIterableDirAbsolute("/", .{ .access_sub_paths = true });
     defer dir.close();
     var dirIterator = dir.iterate();
     while (try dirIterator.next()) |dirContent| {
